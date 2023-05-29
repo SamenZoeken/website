fae4354d5dc95da7f9d89a11f82ca2acb18d9ef9
Ben <ben@kn-app.com>
2023-05-29 17:54:03 +0200

    feat: changed url from github to samenzoeken.app

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,8 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: `https://${organizationName}.github.io`,
+  // url: `https://${organizationName}.github.io`,
+  url: `https://samenzoeken.app`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: `/`,
